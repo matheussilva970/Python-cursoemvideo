907f0e964db6ab3a391d02bf64fdf81cf7c20fb7
matheussilva970 <githubmatheussilva@gmail.com>
2023-05-07 16:32:34 -0300

    Criei o ex008

---
+++ exercicios-python/ex008.py
@@ -0,0 +1,8 @@
+distancia = float(input('Uma distância em metros: '))
+print(f'A medida de {distancia}m corresponde a')
+print(f'{distancia / 1000}km')
+print(f'{distancia / 100}hm')
+print(f'{distancia / 10}dam')
+print(f'{distancia * 10:.0f}dm')
+print(f'{distancia * 100:.0f}cm')
+print(f'{distancia * 1000:.0f}mm')
