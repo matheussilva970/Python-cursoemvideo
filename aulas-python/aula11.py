a44408572fb3b6ccbc423e287c52403cfebbe546
matheussilva970 <githubmatheussilva@gmail.com>
2023-05-11 08:56:37 -0300

    Criei a aula11

---
+++ aulas-python/aula11.py
@@ -0,0 +1,7 @@
+nome = 'Matheus'
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33m',
+         'pretoebranco': '\033[7;30m'}
+
+print(f'Olá! Muito prazer em te conhecer, {cores["pretoebranco"]}{nome}{cores["limpa"]}!!')
